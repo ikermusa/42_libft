3b1dd2e33a1a212eac79f56cad28e89103f58aa2
Iker Musatadi Madariaga <imusatad@c3r5s3.42urduliz.com>
2024-10-01 18:02:44 +0200

    lstmap casi listo

--- Makefile
+++ Makefile
@@ -44,6 +44,8 @@ SRC_BONUS	= ./ft_lstnew_bonus.c		\
 			  ./ft_lstadd_back_bonus.c	\
 			  ./ft_lstdelone_bonus.c	\
 			  ./ft_lstclear_bonus.c		\
+			  ./ft_lstiter_bonus.c		\
+			  ./ft_lstmap_bonus.c		\
 
 
 OBJ = $(SRC:.c=.o)
--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -15,8 +15,48 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_lst;
+	void	*new_content;
 
 	if (!lst || !f || !del)
 		return (NULL);
-	
+	while (lst)
+	{
+		new_content = f(lst->content);
+		new_lst = ft_lstnew(new_content);
+		if (!new_lst)
+		{
+			del(new_content);
+			ft_lstclear(lst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(lst, new_lst);
+		lst = lst->next;
+	}
+	return (new_lst);
+}
+
+t_list	*f(void *content)
+{
+	int	*new_content;
+
+	new_content = malloc(sizeof(int));
+	if (new_content == NULL)
+		return (NULL);
+	*new_content = (*(int *)content) * 2;
+	return (new_content);
+}
+
+void	del(void *content)
+{
+	free(content);
+}
+
+int main(void)
+{
+    t_list *lst = ft_lstnew((void *)(intptr_t)1);
+    ft_lstadd_back(&lst, ft_lstnew((void *)(intptr_t)2));
+    ft_lstadd_back(&lst, ft_lstnew((void *)(intptr_t)3));
+
+    t_list *new_list = ft_lstmap(lst, duplicate_content, free);
+
 }
